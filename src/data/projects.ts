1746aa1786d64f452f628588da6b64acfee95125
Mario <mariomartinmorilla@gmail.com>
2026-09-19 19:21:00 +0200

    routemaster url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,6 +20,7 @@ export const projectsData: Project[] = [
     id: "routemaster",
     technologies: ["Spring Boot", "Java", "Angular", "TypeScript", "CSS", "MySQL", "AWS", "Docker", "Cloudflare", "Bucket S3", "EC2", "CloudFront", "Linux", "Bash", "SSL", "Jenkins"],
     image: routeMasterImage,
-    githubUrl: "https://github.com/IHoracio/TFG-RouteMaster"
+    githubUrl: "https://github.com/IHoracio/TFG-RouteMaster",
+    demoUrl: "https://routemaster.mariomartinmorilla.es"
   }
 ];
